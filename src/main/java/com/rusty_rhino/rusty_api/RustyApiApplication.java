c985fb2be05e52e6c11d2bc8aa57e9ff74549d66
David Larson <dalarson@wpi.edu>
2025-03-10 20:36:16 -0700

    fixes

--- src/main/java/com/rusty_rhino/rusty_api/RustyApiApplication.java
+++ src/main/java/com/rusty_rhino/rusty_api/RustyApiApplication.java
@@ -3,10 +3,13 @@ package com.rusty_rhino.rusty_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
+
 @SpringBootApplication
 public class RustyApiApplication {
 
 	public static void main(String[] args) {
+		ApplicationInsights.attach();
 		SpringApplication.run(RustyApiApplication.class, args);
 	}
 }
